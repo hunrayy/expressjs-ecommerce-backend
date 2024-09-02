4a9662290f5e187b360ed095d3c2f1c2b77580c1
hunrayy <henryokiyi8@gmail.com>
2024-09-02 18:18:36 +0100

    first commit

---
+++ Auth/Auth.js
@@ -0,0 +1,184 @@
+
+
+const crypto = require("crypto")
+const nodemailer = require("nodemailer")
+const dotenv = require("dotenv").config()
+const jwt = require("jsonwebtoken")
+const bcryptjs = require("bcryptjs")
+
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.APP_NAME,
+      pass: process.env.APP_PASSWORD,
+    }
+});
+const verificationCode = () => {
+    return crypto.randomInt(100000, 1000000).toString();
+};;
+
+
+class Auth {
+    constructor(session) {
+        this.session = session; // Pass the session object to the class
+    }
+    // function to check if user exists
+    async checkIfUserExist(email){
+        const feedback = await client.db(process.env.DB_NAME).collection("users").findOne({email: email})
+        if(feedback){return "exists"}else{return "not-exist"}
+    }
+
+    async getUserByEmail(email){
+        const get_user_feedback = await client.db(process.env.DB_NAME).collection("users").findOne({email: email})
+        if(get_user_feedback){
+
+            return {
+                message: "User retrieved by their email ",
+                code: "success",
+                data: get_user_feedback
+            }
+
+        }else{
+
+            return {
+                message: "User's data could not be retrieved",
+                code: 'error',
+                data: null
+
+            }
+        }
+    }
+
+    async createToken(payload, expires){
+    
+        const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: expires })
+    
+        return token;
+    
+    }
+
+    async sendEmailVerificationCode(email){
+        try {
+            const info = await transporter.sendMail({
+                from: `"beautybykiara" <${process.env.APP_NAME}>`, // sender address
+                to: email, // list of receivers
+                subject: "Email Verification Code", // Subject line
+                html: `<h4>Your Email Verification code is ${verificationCode()}</h4>` // html body
+            });
+            if(info){
+                //verification mail sent. next generate a token and send
+                const tokenPayload = {email: email, verificationCode: verificationCode()}
+                const createToken = await this.createToken(tokenPayload, "5m")
+                return({
+                    code: "success",
+                    message: "Email verification code sent successfully",
+                    verificationCode: verificationCode(),
+                    genratedToken: createToken
+                })
+            }
+            
+        }catch(error) {
+            return({
+                code: "error",
+                message: "an error occured while sending verification code",
+                reason: error.message
+            })
+            
+        }
+    }
+
+    async createAccount({firstname, email, password}) {
+        // Check if any field is empty or has length less than 1
+        shortPassword = password.length < 6
+        if (!firstname || !email || !password || firstname.length < 1 || email.length < 1 || password.length < 1) {
+            return ({
+                code: "error",
+                message: shortPassword ? "Length of password must be greater than 6" : "All fields must be filled"
+            });
+        }
+        // Email regex pattern
+        const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+        // Validate email format
+        if (!emailPattern.test(email)) {
+            return ({
+                code: "error",
+                message: "Invalid email format"
+            });
+        }
+      try{
+            const checkIfUserExist = this.checkIfUserExist(email)
+            if(checkIfUserExist == "exists"){
+                return({
+                    code: "error",
+                    message: "Email already in use"
+                })
+            }else{
+                const hashedPassword = await bcryptjs.hash(password, 10)
+                const userDetails = {
+                    firstname: firstname,
+                    email: email,
+                    password: hashedPassword
+                }
+                const createAccount = await client.db(process.env.DB_NAME).collection("users").insertOne(userDetails)
+                return({
+                    message: "Account successfully created",
+                    code: "success",
+                    data: {
+                        firstname: firstname,
+                        email: email
+                    }
+                })
+            }
+        }catch(error){
+            return({
+                code: "error",
+                message: "Account could not be created",
+                reason: error.message
+            })
+        }
+
+    }
+
+    async login({email, password}){
+        const checkIfUserExist = await this.checkIfUserExist(email)
+        if(checkIfUserExist == "not-exist"){
+            return ({
+                message: "Invalid email/password, have you registered?",
+                code: "error"
+            })
+        }
+        // get user's details by email
+        const getUserDetails = await this.getUserByEmail(email)
+        if(getUserDetails.code == "success"){
+            const firstname = getUserDetails.code.firstname
+            const hashedPassword = getUserDetails.data.password
+
+            const verifyPassword = await bcryptjs.compare(password, hashedPassword)
+            if(!verifyPassword){
+                return({
+                    message: "Invalid email/password, have you registered?"
+                })
+            }
+            const payload = {
+                email: email,
+                firstname: firstname
+            }
+            const loginTokin = await this.createToken(payload, null)
+            return({
+                message: "Login success",
+                code: "success",
+                data: {
+                    firstname: firstname,
+                    email: email
+                }
+            })
+        }
+    }
+
+}
+
+module.exports = Auth;
---
+++ server.js
@@ -0,0 +1,119 @@
+const express = require('express');
+const server = express();
+const dotenv = require("dotenv").config();
+const session = require("express-session")
+const jwt = require("jsonwebtoken")
+
+const AuthClass = require('./Auth/Auth')
+const Auth = new AuthClass
+
+
+server.use(express.json())
+server.use(session({
+    secret: process.env.SESSION_SECRET_KEY, // Replace with a strong secret key
+    resave: false,
+    saveUninitialized: true,
+    cookie: { 
+        secure: false, // Set to true if using HTTPS
+        maxAge: 5 * 60 * 1000 // 5 minutes in milliseconds
+    }
+}));
+
+
+const PORT = process.env.PORT
+
+const verifyEmailVerificationToken = (request, response, next) => {
+    try{
+        const token = request.headers.token
+        if(token !== null && typeof(token) !== undefined){
+            const verify = jwt.verify(token, process.env.JWT_SECRET_KEY)
+            request.emailVerificationToken = token
+        }
+
+        next();
+    }catch(error){
+        response.send({
+            message: "invalid jsonwebtoken",
+            code: "error"
+        })
+    }
+
+
+
+}
+server.get("/", (request, response) => {
+    response.send({
+        message: "everything works fine",
+        code: "success"
+    })
+})
+// ------------------------send email verification code------------------------//
+server.post("/send-email-verification-code", async(request, response) => {
+    const email = request.body.email
+    const sendEmailVerificationCode = await Auth.sendEmailVerificationCode(email)
+    if (sendEmailVerificationCode.code == "success"){
+        request.session.emailVerificationCode = sendEmailVerificationCode.verificationCode
+        request.email = email
+
+        response.send({
+            code: "success",
+            message: "Email verification code sent successfully",
+            genratedToken: sendEmailVerificationCode.genratedToken,
+            userEmail: email,
+            code: sendEmailVerificationCode.verificationCode
+        })
+    }else{
+        response.send(sendEmailVerificationCode)
+    }
+})
+// ------------------------verify email verification code------------------------//
+server.post("/verify-email-verification-code", verifyEmailVerificationToken,  async(request, response) => {
+    const verificationCode = request.body.verificationCode
+    console.log("verificationCode: ", verificationCode, "emailVerificationToken: ", request.emailVerificationToken)
+    try{
+        if(request.session.emailVerificationCode == verificationCode){
+            const payload = {email: request.email}
+            const createAccountToken = await Auth.createToken(payload, "20m")
+            response.send({
+                code: "success",
+                message: "Email successfully verified, Proceed to register",
+                createAccountToken: createAccountToken 
+            }) 
+        }
+    }catch(error){
+        response.send({
+            code: "error",
+            message: "invalid verification code"
+        })
+    }
+})
+
+// ------------------------create account------------------------//
+server.post("/register", verifyEmailVerificationToken, async(request, response) => {
+    const {firstname, email, password} = request.body
+    const registerFeedback = await Auth.createAccount({firstname, email, password})
+    try{
+        response.send(registerFeedback)
+    }catch(error){
+        response.send({code: "error", message: "An error occured while creating account", reason: error.message})
+    }
+})
+
+// -----------------------------Login------------------------------//
+server.post("/login", async(request, response) => {
+    const {firstname, email, password} = request.body
+    try{
+        const registerFeedback = await Auth.login({email, password})
+        response.send (registerFeedback)
+    }catch(error){
+        response.send({code: "error", message: "An error occured while creating logging in", reason: error.message})
+    }
+})
+
+
+
+
+
+
+
+server.listen(PORT, ()=> console.log(`server is listening on http://localhost:${PORT}`))
